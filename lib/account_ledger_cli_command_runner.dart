ad8d0422ea6e6bb0579df047beef9138f4415b50
Banee Ishaque K <baneeishaque@gmail.com>
2023-12-20 14:49:49 +0530

    account_ledger_library_dart - Verify Gist (Version 2) Account Ledger

--- lib/account_ledger_cli_command_runner.dart
+++ lib/account_ledger_cli_command_runner.dart
@@ -1,6 +1,8 @@
-import 'package:account_ledger_cli_dart/get_accounts_command.dart';
-import 'package:account_ledger_cli_dart/verify_gist_account_ledger_command.dart';
+import 'package:account_ledger_cli_dart/verify_gist_v2_account_ledger_command.dart';
 import 'package:args/command_runner.dart';
+
+import 'get_accounts_command.dart';
+import 'verify_gist_account_ledger_command.dart';
 
 class AccountLedgerCliCommandRunner extends CommandRunner {
   AccountLedgerCliCommandRunner()
@@ -11,5 +13,6 @@ class AccountLedgerCliCommandRunner extends CommandRunner {
   void addCommand(Command command) {
     super.addCommand(VerifyGistAccountLedgerCommand());
     super.addCommand(GetAccountsAccountLedgerCommand());
+    super.addCommand(VerifyGistV2AccountLedgerCommand());
   }
 }
---
+++ lib/verify_gist_v2_account_ledger_command.dart
@@ -0,0 +1,16 @@
+import 'package:account_ledger_library/account_ledger_cli.dart';
+import 'package:args/command_runner.dart';
+
+class VerifyGistV2AccountLedgerCommand extends Command {
+  @override
+  String get description =>
+      "Verifies Account Ledger Entries in Gist main.txt File (Version 2)";
+
+  @override
+  String get name => "VerifyGistV2";
+
+  @override
+  void run() {
+    verifyAccountLedgerGistInteractive(isVersion2: true);
+  }
+}
