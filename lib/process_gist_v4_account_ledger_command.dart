029fcc592dbc37e2f040e3f6a8f110e36250c1ee
Banee Ishaque K <baneeishaque@gmail.com>
2024-11-17 19:52:11 +0530

    feat: error for process execution

--- lib/process_gist_v4_account_ledger_command.dart
+++ lib/process_gist_v4_account_ledger_command.dart
@@ -4,6 +4,7 @@ import 'package:account_ledger_library/account_ledger_gist_api.dart';
 import 'package:account_ledger_library/account_ledger_gist_api_interactive.dart';
 import 'package:account_ledger_library/models/account_ledger_gist_model_v2.dart';
 import 'package:args/command_runner.dart';
+import 'package:tuple/tuple.dart';
 
 class ProcessGistV4AccountLedgerCommand extends Command {
   @override
@@ -15,20 +16,24 @@ class ProcessGistV4AccountLedgerCommand extends Command {
 
   @override
   Future<void> run() async {
-    await processAccountLedgerGistV2InterActive(
-      AccountLedgerGistV2Model.fromJson(
-        jsonDecode(
-          runAccountLedgerGistV4Operation(
-            actionsBeforeExecution: () {
-              print('Running GistV4 Operation');
-            },
-            actionsAfterExecution: (String result) {
-              // print('Result : $result');
-            },
-          ),
-        ),
-      ),
-      isVersion3: true,
+    Tuple2<bool, String> gistOperationResult = runAccountLedgerGistV4Operation(
+      actionsBeforeExecution: () {
+        print('Running GistV4 Operation');
+      },
+      actionsAfterExecution: (String result) {
+        // print('Result : $result');
+      },
     );
+    if (gistOperationResult.item1) {
+      await processAccountLedgerGistV2InterActive(
+        AccountLedgerGistV2Model.fromJson(
+          jsonDecode(gistOperationResult.item2),
+        ),
+        isVersion3: true,
+      );
+    } else {
+      print("Gist Operation Failure...");
+      print(gistOperationResult.item2);
+    }
   }
 }
